e351b3f8f65bdeeaacdf43e2f974b9cda06611fd
Kasper Meerts <kaspermeerts@gmail.com>
2011-12-08 00:27:44 +0100

    I'm a gigantic idiot

--- matrix.c
+++ matrix.c
@@ -111,9 +111,11 @@ void mat4_copy(Mat4 dst, Mat4 src)
 void mat4_transpose(Mat4 m)
 {
 #define M(i,j) m[4*j + i]
-	M(0,1) = M(1,0); M(0,2) = M(2,0); M(0,3) = M(3,0);
-	                 M(1,2) = M(2,1); M(1,3) = M(3,1);
-	                                  M(2,3) = M(3,2);
+#define SWAP(i,j) do {double d;d = M(i,j);M(i,j) = M(j,i);M(j,i) = d;} while(0)
+	SWAP(0,1); SWAP(0,2); SWAP(0,3);
+	           SWAP(1,2); SWAP(1,3);
+	                      SWAP(2,3);
+#undef SWAP
 #undef M
 }
 void mat4_mult(Mat4 c, Mat4 a, Mat4 b)
